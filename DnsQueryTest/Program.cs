2d297473b50b33d8d937f0e5c8c87aeabef84b49
un4ckn0wl3z <slickerth@gmail.com>
2022-01-07 22:31:24 +0700

    first commit

---
+++ DnsQueryTest/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DnsQueryTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dn = "checkip.dyndns.org";
+            Console.ReadKey();
+            while (true)
+            {
+                using (WebClient client = new WebClient())
+                {
+                    client.Headers["User-Agent"] = "Mozilla/4.0 (Compatible; Windows NT 5.1; MSIE 6.0)";
+
+                    // Download data.
+                    string value = client.DownloadString("http://" + dn);
+
+                    // Write values.
+                    Console.WriteLine("--- WebClient result ---");
+                    Console.WriteLine(value.Length);
+                    Console.WriteLine(value);
+                }
+                Console.ReadKey();
+            }
+
+
+        }
+    }
+}
